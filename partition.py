8c81ad475798be51d97750d1c054489b3c297269
Shirley Zhu <44847817+shirleymango@users.noreply.github.com>
2022-04-24 10:31:47 -0400

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,194 +1,201 @@
-import random
-from re import L
+from re import I
 import sys
+#import numpy as np
+import random
+import math
+from sys import getsizeof
+import csv
+import timeit
 
-# helper function
-def calcRes(A, s):
-    l = len(A)
-    res = 0
-    for i in range(l):
-        res += A[i]*s[i]
-    return res
+def T(iter):
+    return (10**10) * (0.8 ** math.floor(iter/300))
 
-def kk(A):
-    heap = max_heap(101)
-    for i in A:
-        heap.push(i)
-    for j in range(len(A)-1):
-        largest = heap.pop()
-        secondLargest = heap.pop()
-        heap.push(largest - secondLargest)
-    return largest - secondLargest
+def kama_karp(s):
+    seq = s.copy()
+    y = len(seq)-1
+    for x in range(y):
+        seq.sort(reverse=True)
+        first = seq.pop(0)
+        second = seq.pop(0)
+        difference = abs(first - second)
+        seq.append(difference)
+    seq.sort(reverse=True)
+    return abs(seq[0])
 
-def repeatRandom(A, max_iter):
-    l = len(A)
-    # random solution S
-    s = []
-    sum1 = 0
-    sum2 = 0
-    for j in range(l):
-            rand = random()
-            if rand < 0.5:
-                sum1 += A[j]
-                s[j] = -1
-            else:
-                sum2 += A[j]
-                s[j] = 1
-    bestRes = abs(sum1-sum2)
-    solution = s
-    
-    for i in range(max_iter):
-        # generating a new potential solution
-        for j in range(l):
-            rand = random()
-            if rand < 0.5:
-                sum1 += A[j]
-                s[j] = -1
-            else:
-                sum2 += A[j]
-                s[j] = 1
-        # calculate new res
-        newRes = abs(sum1-sum2)
+def partition_find_P(seq):
+    P = []
+    for i in range(len(seq)):
+        P.append(random.randrange(len(seq)))
+    return P 
 
-        # update best res
-        if newRes < bestRes:
-            bestRes = newRes
-            solution = s
-    return solution
+def partition_find_A(seq,P):
+    A = [0]*len(seq)
+    for x in range(len(seq)):
+        A[P[x]] = A[P[x]] + seq[x]
+    return A
+        
+def randomreg(seq, iter):
+    original = []
+    for j in range(len(seq)):
+        original.append(seq[j] if random.random() < 0.5 else -seq[j])
+    smallest = abs(sum(original))
+    smallestlist = original #finds list that yields smallest residue
+    for i in range(iter):
+        S = []
+        for j in range(len(seq)):
+            S.append(seq[j] if random.random() < 0.5 else -seq[j])
+        comp = abs(sum(S))
+        if(comp<smallest):
+            smallest = comp
+            smallestlist = S
+    #return smallestlist, [m/n for m, n in zip(smallestlist, seq)]
+    return smallest
 
-def hillClimbing(A, max_iter):
-    solution = []
-    l = len(A)
-    # generate random solution
-    s = []
-    sum1 = 0
-    sum2 = 0
-    for j in range(l):
-        rand = random()
-        if rand < 0.5:
-            sum1 += A[j]
-            s[j] = -1
+        
+def randomregpart(seq, iter):
+    smallest = kama_karp(partition_find_A(seq,partition_find_P(seq)))#finds list that yields smallest residue
+    for i in range(iter):
+        comp = kama_karp(partition_find_A(seq,partition_find_P(seq)))
+        if(comp<smallest):
+            smallest = comp
+    #return smallestlist, [m/n for m, n in zip(smallestlist, seq)]
+    return smallest
+
+def hillclimbing(seq, iter):
+    original = []
+    for j in range(len(seq)):
+        original.append(seq[j] if random.random() < 0.5 else -seq[j])
+    smallest = abs(sum(original))
+    smallestlist = original 
+    for i in range(iter):
+        S = smallestlist.copy()
+        randomlist = random.sample(range(0, len(seq)), 2)
+        index1 = randomlist[0]
+        index2 = randomlist[1]
+        S[index1] = -1*smallestlist[index1]
+        prob = random.random()
+        if(prob < 0.5):
+            S[index2] = -1*smallestlist[index2]
+        comp = abs(sum(S))
+        if(comp<smallest):
+            smallest = comp
+            smallestlist = S
+    #return smallestlist, [m/n for m, n in zip(smallestlist, seq)]
+    return smallest
+
+def hillclimbingpart(seq, iter):
+    P = partition_find_P(seq)
+    smallestlist = partition_find_A(seq,P)
+    smallest = kama_karp(smallestlist)
+    for i in range(iter):
+        new_P = P.copy()
+        check = True
+        while(check):
+            rand_j = random.randrange(len(seq))
+            rand_i = random.randrange(len(seq))
+            if(P[rand_i]!=rand_j):
+                P[rand_i]=rand_j
+                check=False
+        comp = kama_karp(partition_find_A(seq,new_P))
+        if(comp<smallest):
+            smallest = comp
+            smallestlist = partition_find_A(seq,new_P)
+    #return smallestlist, [m/n for m, n in zip(smallestlist, seq)]
+    return smallest
+
+def sim_annealing(seq, iter):
+    original = []
+    for j in range(len(seq)):
+        original.append(seq[j] if random.random() < 0.5 else -seq[j])
+    smallest = abs(sum(original))
+    smallestlist = original 
+    s_double_prime = original
+    smallest_double = smallest
+    for i in range(iter):
+        S = smallestlist.copy()
+        randomlist = random.sample(range(0, len(seq)), 2)
+        index1 = randomlist[0]
+        index2 = randomlist[1]
+        S[index1] = -1*smallestlist[index1]
+        prob = random.random()
+        if(prob < 0.5):
+            S[index2] = -1*smallestlist[index2]
+        res = abs(sum(S))
+        if(res<smallest):
+            smallest = res
+            smallestlist = S
         else:
-            sum2 += A[j]
-            s[j] = 1
-    bestRes = abs(sum1-sum2)
-    solution = s
+            if random.random() < math.exp(-(res-smallest)/T(i)):
+                smallest = res
+                smallestlist = S
+        if(res<smallest_double):
+            smallest_double = res
+            s_double_prime = S
+    #return smallestlist, [m/n for m, n in zip(smallestlist, seq)]
+    return smallest_double
 
-    for i in range(max_iter):
-        # generate a random neighbor of S (a neighbor differs from S in either one or two places)
-        rand = random()
-        newS = s
-        if rand < 0.5:
-            # differ in one place
-            index = random.randint(0, l)
-            newS[index] = newS[index]*-1
+def sim_annealing_part(seq, iter):
+    P = partition_find_P(seq)
+    smallestlist = partition_find_A(seq,P)
+    smallest = kama_karp(smallestlist)
+    s_double_prime = smallestlist
+    smallest_double = smallest
+    for i in range(iter):
+        new_P = P.copy()
+        check = True
+        while(check):
+            rand_j = random.randrange(len(seq))
+            rand_i = random.randrange(len(seq))
+            if(P[rand_i]!=rand_j):
+                P[rand_i]=rand_j
+                check=False
+        res = kama_karp(partition_find_A(seq,new_P))
+        if(res<smallest):
+            smallest = res
+            smallestlist = partition_find_A(seq,new_P)
         else:
-            #differ in two places
-            index1 = random.randint(0, l)
-            index2 = index1
-            while index1 == index2:
-                index2 = random.randint(0, l)
-            newS[index1] = newS[index1]*-1
-            newS[index2] = newS[index2]*-1
-        # calculate new residue
-        newRes = calcRes(A, newS)
-        if newRes < bestRes:
-            bestRes = newRes
-            solution = newS
-    return solution
-
-def simulatedAnnealing():
-    return 0
-
-# prepartitioning
-def prepartitioning(A, P):
-    newA = []
-    n = len(P)
-    for j in range(n):
-        newA[P[j]] = newA[P[j]] + A[j]
-    return newA
-
-    # MAX HEAP DATA STRUCTURE
-    # defining a class max_heap for the heap data structure
-class max_heap: 
-    def __init__(self, sizelimit):
-        self.sizelimit = sizelimit
-        self.cur_size = 0
-        self.Heap = [0]*(self.sizelimit + 1)
-        self.Heap[0] = sys.maxsize
-        self.root = 1
- 
- 
-    def swapnodes(self, node1, node2):
-        self.Heap[node1], self.Heap[node2] = self.Heap[node2], self.Heap[node1]
-  
-    # THE MAX_HEAPIFY FUNCTION
-    def max_heapify(self, i):
-  
-        # If the node is a not a leaf node and is lesser than any of its child
-        if not (i >= (self.cur_size//2) and i <= self.cur_size):
-            if (self.Heap[i] < self.Heap[2 * i]  or  self.Heap[i] < self.Heap[(2 * i) + 1]): 
-                if self.Heap[2 * i] > self.Heap[(2 * i) + 1]:
-     # Swap the node with the left child and call the max_heapify function on it
-                    self.swapnodes(i, 2 * i)
-                    self.max_heapify(2 * i)
-  
-                else:
-                # Swap the node with right child and then call the max_heapify function on it
-                    self.swapnodes(i, (2 * i) + 1)
-                    self.max_heapify((2 * i) + 1)
-  
- 
- 
-    # push function
-    def push(self, element):
-        if self.cur_size >= self.sizelimit :
-            return
-        self.cur_size+= 1
-        self.Heap[self.cur_size] = element 
-        current = self.cur_size
-        while self.Heap[current] > self.Heap[current//2]:
-            self.swapnodes(current, current//2)
-            current = current//2
-  
-  
-    # pop function
-    def pop(self):
-        last = self.Heap[self.root]
-        self.Heap[self.root] = self.Heap[self.cur_size]
-        self.cur_size -= 1
-        self.max_heapify(self.root)
-        return last
-  
-  
-    # build function
-    def build(self): 
-        for i in range(self.cur_size//2, 0, -1):
-            self.max_heapify(i)
+            if random.random() < math.exp(-(res-smallest)/T(i)):
+                smallest = res
+                smallestlist = partition_find_A(seq,new_P)
+        if(res<smallest_double):
+            smallest_double = res
+            s_double_prime = partition_find_A(seq,new_P)
+    #return smallestlist, [m/n for m, n in zip(smallestlist, seq)]
+    return smallest_double
 
 def main():
+    # # get arguments from command line for alg_code and input
     alg_code = int(sys.argv[2])
-    inputfile = sys.argv[3]
-    with open(inputfile) as f:
+    input_file = sys.argv[3]
+     # read in input file
+    with open(input_file) as f:
         lines = f.readlines()
     given_seq = []
     for line in lines:
         given_seq.append(int(line))
-    if alg_code == 0:
-        print(kk(given_seq))
-    elif alg_code == 1:
-        print(repeatRandom(given_seq, 25000))
-    elif alg_code == 2:
-        print(hillClimbing(given_seq, 25000))
-    elif alg_code == 3:
-        # simulated annealing
-        print(0)
-    elif alg_code == 11:
-        # prepartitioned repeated random
-        print(0)
-    elif alg_code == 12:
-        # prepartitioned hill clmbing
-        print(0)
-    elif alg_code == 13:
-        # prepartitioned sim annealing
-        print(0)
+    if alg_code==0:
+        #given_seq = [10,8,7,6,5]
+        print(kama_karp(given_seq))
+        #print("hi")
+    elif alg_code==1:
+        #given_seq = [10,8,7,6,5]
+        #print(partition(given_seq))
+        print(randomreg(given_seq,25000))
+        #print("hi")
+    elif alg_code==2:
+        print(hillclimbing(given_seq,25000))
+    elif alg_code==3:
+        #given_seq = [10,8,7,6,5]
+        print(sim_annealing(given_seq,25000))
+    elif alg_code==11:
+        print(randomregpart(given_seq,25000))
+    elif alg_code==12:
+        #given_seq = [10,8,7,6,5]
+        print(hillclimbingpart(given_seq,25000))
+    elif alg_code==13:
+        #given_seq = [10,8,7,6,5]
+        print(sim_annealing_part(given_seq,25000))
+#     elif alg_code==14:
+
+if __name__ == "__main__":
+    main()
